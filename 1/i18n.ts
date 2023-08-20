ef149d19e53a4defd868902a79d76b23c91d675c
Adriano Raiano <adriano@raiano.ch>
2023-08-20 11:04:00 +0200

    update deps

--- 1/i18n.ts
+++ 1/i18n.ts
@@ -1,6 +1,7 @@
 import i18next from 'i18next';
 import enNs1 from './locales/en/ns1.json';
 import enNs2 from './locales/en/ns2.json';
+import enFallback from './locales/en/fallback.json';
 import deNs1 from './locales/de/ns1.json';
 import deNs2 from './locales/de/ns2.json';
 
@@ -16,6 +17,7 @@ i18next.init({
     en: {
       ns1: enNs1,
       ns2: enNs2,
+      fallback: enFallback,
     },
     de: {
       ns1: deNs1,
